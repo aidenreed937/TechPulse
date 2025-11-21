1004ec00a94d151d520d7d0be66fb41caa528049
aidenreed937 <aidenreed937@gmail.com>
2025-11-22 00:10:29 +0800

    style(shared): normalize formatting

--- shared/src/jsMain/kotlin/com/aidenreed/techpulse/Platform.js.kt
+++ shared/src/jsMain/kotlin/com/aidenreed/techpulse/Platform.js.kt
@@ -1,6 +1,6 @@
 package com.aidenreed.techpulse
 
-class JsPlatform: Platform {
+class JsPlatform : Platform {
     override val name: String = "Web with Kotlin/JS"
 }
 
--- shared/src/wasmJsMain/kotlin/com/aidenreed/techpulse/Platform.wasmJs.kt
+++ shared/src/wasmJsMain/kotlin/com/aidenreed/techpulse/Platform.wasmJs.kt
@@ -1,6 +1,6 @@
 package com.aidenreed.techpulse
 
-class WasmPlatform: Platform {
+class WasmPlatform : Platform {
     override val name: String = "Web with Kotlin/Wasm"
 }
 
