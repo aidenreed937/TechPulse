0eaf69d01d43d48cba15cd62b4d4fbcfe4b27165
aidenreed937 <aidenreed937@gmail.com>
2025-11-22 00:10:52 +0800

    feat(server): use shared greeting and constants

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,7 +8,7 @@ group = "com.aidenreed.techpulse"
 version = "1.0.0"
 application {
     mainClass.set("com.aidenreed.techpulse.ApplicationKt")
-    
+
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
